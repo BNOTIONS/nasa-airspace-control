9c50ee4c62a6877193d17aef338f528c6f65e6da
Pirdad <pirdod@gmail.com>
2013-04-20 19:24:15 -0400

    Created the adapter for alerts and the listview for it and the inflating of each item works.

--- src/com/bnotions/airspacecontrol/HomeActivity.java
+++ src/com/bnotions/airspacecontrol/HomeActivity.java
@@ -2,13 +2,56 @@ package com.bnotions.airspacecontrol;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import com.bnotions.airspacecontrol.adapter.AlertsAdapter;
+import com.bnotions.airspacecontrol.entity.Alert;
+
+import java.util.ArrayList;
+
 
 public class HomeActivity extends Activity {
+
+    private ListView listview_alerts;
+    private AlertsAdapter adapter_alerts;
 
     @Override
     public void onCreate(Bundle state) {
 
         super.onCreate(state);
         setContentView(R.layout.activity_main);
+        
+        initializeUi();
+    }
+
+    private void initializeUi() {
+        
+        adapter_alerts = new AlertsAdapter(this, getTestAlerts());
+        listview_alerts = (ListView) findViewById(R.id.listview_alerts);
+        listview_alerts.setAdapter(adapter_alerts);
+    }
+
+    private ArrayList<Alert> getTestAlerts() {
+
+        ArrayList<Alert> list_alerts = new ArrayList<Alert>();
+        list_alerts.add(new Alert(12, "When the lights are off, get under the desk.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "When the desks are flipped, hide behind it.", Alert.STATUS_RED));
+        list_alerts.add(new Alert(12, "When there's a hole in the desk, panic.", Alert.STATUS_RED));
+        list_alerts.add(new Alert(12, "If there's a black desk, it's better to hide there.", Alert.STATUS_GREEN));
+        list_alerts.add(new Alert(12, "Turn off your cell phone or any electronic device.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "Do not Scream.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "When the lights are off, get under the desk.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "When the desks are flipped, hide behind it.", Alert.STATUS_RED));
+        list_alerts.add(new Alert(12, "When there's a hole in the desk, panic.", Alert.STATUS_RED));
+        list_alerts.add(new Alert(12, "If there's a black desk, it's better to hide there.", Alert.STATUS_GREEN));
+        list_alerts.add(new Alert(12, "Turn off your cell phone or any electronic device.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "Do not Scream.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "When the lights are off, get under the desk.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "When the desks are flipped, hide behind it.", Alert.STATUS_RED));
+        list_alerts.add(new Alert(12, "When there's a hole in the desk, panic.", Alert.STATUS_RED));
+        list_alerts.add(new Alert(12, "If there's a black desk, it's better to hide there.", Alert.STATUS_GREEN));
+        list_alerts.add(new Alert(12, "Turn off your cell phone or any electronic device.", Alert.STATUS_ORANGE));
+        list_alerts.add(new Alert(12, "Do not Scream.", Alert.STATUS_ORANGE));
+
+        return list_alerts;
     }
 }
---
+++ src/com/bnotions/airspacecontrol/entity/Alert.java
@@ -0,0 +1,56 @@
+package com.bnotions.airspacecontrol.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pirdod
+ * Date: 4/20/13
+ * Time: 6:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Alert {
+
+    public static final int STATUS_GREEN = 223356;
+    public static final int STATUS_ORANGE = 223367;
+    public static final int STATUS_RED = 223378;
+
+    private int id;
+    private String text;
+    private int status_color;
+
+    public Alert(int id, String text, int status_color) {
+
+        this.id = id;
+        this.text = text;
+        this.status_color = status_color;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getText() {
+
+        return text;
+    }
+
+    public void setText(String text) {
+
+        this.text = text;
+    }
+
+    public int getStatusColor() {
+
+        return status_color;
+    }
+
+    public void setStatusColor(int status_color) {
+
+        this.status_color = status_color;
+    }
+}
