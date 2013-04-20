ef5c78fd995757a9a5f32fb0855d0812f82ab21b
Matthew Patience <matthewj.patience@gmail.com>
2013-04-19 23:30:20 -0400

    first commit

---
+++ src/com/bnotions/airspacecontrol/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.bnotions.airspacecontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+
+
+
+    }
+
+}
