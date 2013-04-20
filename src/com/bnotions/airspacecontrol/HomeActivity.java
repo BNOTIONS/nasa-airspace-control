e3cf7ed55f64280ab558030546baf6aaaba75250
Pirdad <pirdod@gmail.com>
2013-04-20 18:00:17 -0400

    used setcontentview in HomeActivity to display the layout.

--- src/com/bnotions/airspacecontrol/HomeActivity.java
+++ src/com/bnotions/airspacecontrol/HomeActivity.java
@@ -9,9 +9,6 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle state) {
 
         super.onCreate(state);
-
-
-
+        setContentView(R.layout.activity_main);
     }
-
 }
