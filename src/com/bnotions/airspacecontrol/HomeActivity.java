b3370a0116f87dcdba8c7459745cc56a766d436e
Matthew Patience <matthewj.patience@gmail.com>
2013-04-21 00:57:51 -0400

    Added AirportView to Activity

--- src/com/bnotions/airspacecontrol/HomeActivity.java
+++ src/com/bnotions/airspacecontrol/HomeActivity.java
@@ -1,10 +1,14 @@
 package com.bnotions.airspacecontrol;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import com.bnotions.airspacecontrol.adapter.AlertsAdapter;
 import com.bnotions.airspacecontrol.entity.Alert;
+import com.bnotions.airspacecontrol.view.airport.AirportView;
 
 import java.util.ArrayList;
 
@@ -13,10 +17,11 @@ public class HomeActivity extends Activity {
 
     private ListView listview_alerts;
     private AlertsAdapter adapter_alerts;
+    private FrameLayout layout_canvas;
+    private AirportView airport;
 
     @Override
     public void onCreate(Bundle state) {
-
         super.onCreate(state);
         setContentView(R.layout.activity_main);
         
@@ -28,6 +33,11 @@ public class HomeActivity extends Activity {
         adapter_alerts = new AlertsAdapter(this, getTestAlerts());
         listview_alerts = (ListView) findViewById(R.id.listview_alerts);
         listview_alerts.setAdapter(adapter_alerts);
+        layout_canvas = (FrameLayout) findViewById(R.id.layout_canvas);
+
+        airport = new AirportView(this, null);
+        layout_canvas.addView(airport, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
     }
 
     private ArrayList<Alert> getTestAlerts() {
